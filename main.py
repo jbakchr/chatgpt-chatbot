08f9cb97c29bcb29a2d71cf48aa2630de7ed2ec4
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-10-13 13:45:31 +0200

    Added example of how to use chat gpt

---
+++ main.py
@@ -0,0 +1,19 @@
+import openai
+
+openai.api_key = ""  # Insert your own API key here
+
+
+def gpt_chat_bot(msg):
+    answer = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo", messages=[{"role": "user", "content": msg}]
+    )
+
+    return answer.choices[0].message.content.strip()
+
+
+if __name__ == "__main__":
+    while True:
+        user_input = input("Your input to ChatGPT: ")
+
+        response = gpt_chat_bot(user_input)
+        print(f"Chatbot: {response}")
